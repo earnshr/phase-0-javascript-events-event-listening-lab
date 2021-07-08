dbcc4893605a9854494ced49a92f3d33b9d71ed7
bexearn12 <earnshr@gmail.com>
2021-07-08 11:22:22 -0500

    commit tree 0621c216d95b568a93484da9279daf55edd93e20

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-function addingEventListener() {
-    
+function addingEventListener(){
     const input = document.getElementById('input');
+    const div = document.getElementByClass('div', [4]);
 
-    function clickAlert()
-    {
-        alert('I was clicked');
+    function clickAlert() {
+        alert('I was clicked!');
     }
-
+    
     input.addEventListener('click', clickAlert);
 }
+
 addingEventListener();
+
+
